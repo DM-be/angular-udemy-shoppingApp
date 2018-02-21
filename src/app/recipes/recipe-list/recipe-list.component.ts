1d8ddf4ae6d419b6a6a262e0107082ea85610578
MorentDennis <dennis.morent@student.howest.be>
2018-02-21 16:12:51 +0100

    replaced event chaining

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -11,17 +11,8 @@ import { RecipeService } from '../recipe.service';
 export class RecipeListComponent implements OnInit {
   
   recipes: Recipe[];
-  
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
 
-  }
-
-  constructor(private recipeService: RecipeService) {
-
-   }
+  constructor(private recipeService: RecipeService) {}
     
   ngOnInit() {
     this.recipes = this.recipeService.getRecipes();
